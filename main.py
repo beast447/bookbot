e4881ece31a3cc257772b9ec4860c766485cabcc
Trevor King <wildbeast447@gmail.com>
2025-11-11 13:20:04 -0500

    Added functions to main.py to read and display contents of a book to the command line.

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
-print("greetings boots")
+def get_book_text(file):
+    with open(file) as f:
+        file_contents = f.read()
+    return file_contents
+
+def main():
+    contents = get_book_text("books/frankenstein.txt")
+    print(contents)
+
+main()
