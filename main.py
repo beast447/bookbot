bba5a60a41bda339e822690aad0111542c063668
Trevor King <wildbeast447@gmail.com>
2025-11-12 07:53:58 -0500

    Added a stats.py file to hold data analysis logic

--- main.py
+++ main.py
@@ -1,12 +1,9 @@
+from stats import count_words
+
 def get_book_text(file):
     with open(file) as f:
         file_contents = f.read()
     return file_contents
-
-def count_words(text):
-    split_string = text.split()
-    word_count = len(split_string)
-    return word_count
 
 def main():
     num_words = count_words(get_book_text("books/frankenstein.txt"))
---
+++ stats.py
@@ -0,0 +1,4 @@
+def count_words(text):
+    split_string = text.split()
+    word_count = len(split_string)
+    return word_count
