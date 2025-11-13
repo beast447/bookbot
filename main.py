8b69c1e9beb63ae3d2c83612cf05d3ebc4797e5a
Trevor King <wildbeast447@gmail.com>
2025-11-13 13:03:12 -0500

    Added the ability to count each and every character and display to the command line in a prettier sorted format

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 from stats import count_words
 from stats import count_chars
+from stats import sort_on
+from stats import sort_dict
+
 def get_book_text(file):
     with open(file) as f:
         file_contents = f.read()
@@ -8,8 +11,15 @@ def get_book_text(file):
 def main():
     num_words = count_words(get_book_text("books/frankenstein.txt"))
     num_chars = count_chars(get_book_text("books/frankenstein.txt"))
+    sorted_dict = sort_dict(num_chars)
+    print("============ BOOKBOT ============")
+    print("Analyzing book found at books/frankenstein.txt...")
+    print("----------- Word Count -----------")
     print(f"Found {num_words} total words")
-    print(f"{num_chars}")
-    
-
+    print("--------- Character Count ---------")
+    for item in sorted_dict:
+        ch = item["char"]
+        if ch.isalpha():
+            print(f"{ch}: {item['num']}")
+    print("============= END =============")
 main()
--- stats.py
+++ stats.py
@@ -12,3 +12,18 @@ def count_chars(text):
         else:
             char_dict[char] += 1
     return char_dict
+
+def sort_dict(dictionary):
+    list_of_dicts = []
+    for key in dictionary:
+        count = dictionary[key]
+        entry = {}
+        entry["char"] = key
+        entry["num"] = count
+        list_of_dicts.append(entry)
+    list_of_dicts.sort(key=sort_on, reverse=True)
+    return list_of_dicts
+
+def sort_on(dictionary):
+    return dictionary["num"]
+
