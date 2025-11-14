0dc53ecda127936d559686c7664f1d90715442e9
Trevor King <wildbeast447@gmail.com>
2025-11-14 16:24:59 -0500

    Replaced the hardcoded values with a sys.argv as a path to the book file

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-from stats import count_words
-from stats import count_chars
-from stats import sort_on
-from stats import sort_dict
+from stats import count_words, count_chars, sort_on, sort_dict
+import sys
 
 def get_book_text(file):
     with open(file) as f:
@@ -9,17 +7,21 @@ def get_book_text(file):
     return file_contents
 
 def main():
-    num_words = count_words(get_book_text("books/frankenstein.txt"))
-    num_chars = count_chars(get_book_text("books/frankenstein.txt"))
-    sorted_dict = sort_dict(num_chars)
-    print("============ BOOKBOT ============")
-    print("Analyzing book found at books/frankenstein.txt...")
-    print("----------- Word Count -----------")
-    print(f"Found {num_words} total words")
-    print("--------- Character Count ---------")
-    for item in sorted_dict:
-        ch = item["char"]
-        if ch.isalpha():
-            print(f"{ch}: {item['num']}")
-    print("============= END =============")
+    if len(sys.argv) < 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+    else:
+        num_words = count_words(get_book_text(sys.argv[1]))
+        num_chars = count_chars(get_book_text(sys.argv[1]))
+        sorted_dict = sort_dict(num_chars)
+        print("============ BOOKBOT ============")
+        print("Analyzing book found at books/frankenstein.txt...")
+        print("----------- Word Count -----------")
+        print(f"Found {num_words} total words")
+        print("--------- Character Count ---------")
+        for item in sorted_dict:
+            ch = item["char"]
+            if ch.isalpha():
+                print(f"{ch}: {item['num']}")
+        print("============= END =============")
 main()
